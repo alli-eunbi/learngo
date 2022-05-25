0f6dfa464abe1f5e9ed49a8e5b8f27b6985cd989
Alli_Eunbi <allieunbi@gmail.com>
2022-05-25 14:38:52 +0900

    매개변수 활용법

--- main.go
+++ main.go
@@ -5,7 +5,21 @@ import (
 	"main/something"
 )
 
+func nameLen(name string) (string, int){
+	return name, len(name)
+}
+
+func allNameLen(name ...string){
+	fmt.Println(name)
+}
+
+
 func main(){
-	fmt.Println("hello")
 	something.SayHello()
+	num := 1
+	fmt.Println(num)
+	fmt.Println(nameLen("alli"))
+	name, _:= nameLen("alli")
+	fmt.Println(name)
+	allNameLen("alli", "cj", "jerry", "eunbi")
 }
