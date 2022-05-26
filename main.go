de59eeb86112234636462b85a6f3fe71aedf972a
Alli_Eunbi <allieunbi@gmail.com>
2022-05-26 15:33:28 +0900

    배열과 slice, map과 struct 연습

--- main.go
+++ main.go
@@ -54,6 +54,27 @@ func pointer(num int) (int, int, *int,int){
 	return a, b, c, d
 }
 
+func array(num ...int) []int{
+	number := num
+	number = append(number, 4)
+	return number
+}
+
+func mapOrStruct(name string, phoneNumber int, married bool)(map[string]string, struct{name string; phoneNumber int; married bool}) {
+	thisMap := map[string]string{name:name}
+	
+	type forStruct struct{
+		name string 
+		phoneNumber int
+		married bool
+	}
+
+	thisStruct := forStruct{name:name, phoneNumber:phoneNumber, married: married}
+		
+	
+	return thisMap,thisStruct
+}
+
 
 func main(){
 	// something.SayHello()
@@ -67,7 +88,9 @@ func main(){
 	// fmt.Println(forArray(1,2,3))
 	// fmt.Println(areYouRich(6000))
 	// fmt.Println(switchCases(20))
-	
-	fmt.Println(pointer(1))
+	//fmt.Println(pointer(1))
+	// fmt.Println(array(1,2,3))
+
+	fmt.Println(mapOrStruct("alli", 1234, false))
 
 }
